9828c8b0c62ec56901cbd746b2a1ce94e5abc77d
Trung <trongtrung17121@gmail.com>
2026-06-12 12:03:16 +0700

    auto-commit

--- TRR/optap.cpp
+++ TRR/optap.cpp
@@ -9,60 +9,70 @@ using namespace std ;
 
 // mảng parent dùng để truy viết parent [105] = {0}
 
-int n , m , d ;
-int parent [105];
-struct ed {
-    int u , v , w;
+int n, d, m;
+struct Edge{
+  int u;
+  int v;
+  int w;
 };
-vector <ed> T, V;
-int cmp (ed a , ed b){
-    if (a.w == b.w) {
-        if (a.u == b.u){
-            return a.v < b.v;
-        }
-        return a.u < b.u;
-    }
-    return a.w < b.w;
+vector<Edge> V, T;
+int cmp(Edge e1, Edge e2) {
+	if (e1.w == e2.w) {
+		if (e1.u == e2.u) return e1.v < e2.v;
+		return e1.u < e2.u;
+	}
+    return e1.w < e2.w;
 }
-int Find (int u ){
-    if (parent [u] == u) return u;
-    return parent [u] = Find (parent [u]);
+int parent [105];
+int Find(int u) {
+    if (parent[u] == u) return u;
+    return parent[u] = Find(parent[u]);
+}
 
-}
-int Union (int u , int v){
-    u = Find (u);
-    v = Find (v);
+bool Union(int u, int v) {
+    u = Find(u);
+    v = Find(v);
+
     if (u == v) return false;
-    parent [v] = u;
-    return  true;
+
+    parent[v] = u;
+    return true;
 }
-void kruskal (){
-    d =  0;
-    T.clear ();
-    sort (V.begin () , V.end() , cmp);
-    for (int i = 1; i<= n ;i++){
+
+void Kruskal() {
+    d = 0;
+    T.clear();
+
+    sort(V.begin(), V.end(), cmp);
+
+    for (int i = 1; i <= n; i++)
         parent[i] = i;
-    }
-    for (ed e : V){
-        if (Union (e.u , e.v)){
-            d+= e.w;
-            T.push_back (e);
-            if (T.size () == n-1) break;
+
+    for (Edge e : V) {
+        if (Union(e.u, e.v)) {
+            T.push_back(e);
+            d += e.w;
+
+            if (T.size() == n - 1)
+                break;
         }
     }
 }
-int main (){
-    cin >> n >> m;
-    V.clear () ; T.clear();
-    for (int i = 0 ; i< m ; i++){
-        ed e;
-        cin >>e .u >> e.v >> e.w ;
-        V.push_back (e);
-    }
-    kruskal ();
-    cout <<d << endl;
-    for (int i = 0 ; i< T.size () ; i++){
-       cout << T[i].u << " " << T[i].v <<  " " << T[i].w << endl;
-    }
-}
 
+int main() {
+    
+	V.clear(); T.clear();
+    cin >> n;
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+      Edge e;
+      cin >> e.u >> e.v >> e.w;
+      V.push_back(e);
+    }
+    Kruskal();
+    cout << d << endl;
+    for (int i = 0; i < T.size(); i++) {
+        cout << T[i].u << " " << T[i].v <<  " " << T[i].w << endl;
+    }
+
+}
