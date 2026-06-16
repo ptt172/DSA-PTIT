2b664e985d9527ef03b58de982e4750501c80d58
Trung <trongtrung17121@gmail.com>
2026-06-16 22:38:10 +0700

    auto-commit

--- Dsa/optap.cpp
+++ Dsa/optap.cpp
@@ -2,35 +2,26 @@
 using namespace std;
 
 
-int n;
-int a[105];
-int stt = 1;
-bool vs[105];
-void in (){
-    cout << stt << ": ";
-    stt ++;
-    
-    for (int i = 1; i <= n ; i++ ){
-        cout << a[i] << " ";
-    }
-    cout << endl;
-}
-void ql (int i){
-    for (int j = 1; j<= n ;j++){
-        if (!vs[j]){
-            a[i] = j;
-            vs[j] = true;
-            if (i == n) {
-                in ();
-            }
-            else {
-                ql (i + 1);
-            }
-            vs[j] = false;
-        } 
-    }
-}
 int main (){
-    cin >> n;
-    ql (1);
+    int t , n , k;
+    cin >> t;
+    while (t--){
+        cin >> n>> k;
+        int a[105];
+        for (int i = 1 ; i <= k;i++){
+			cin >> a[i];
+		}
+        int i = k;
+        while (a[i] == n - k + i) i --;
+        if (i == 0) a[i] = 0;
+        else a[i] ++;
+        while ( i < k) {
+            a[i + 1] = a[i] + 1;
+            i ++;
+        }
+        for (int i = 1 ; i <= k;i++){
+			cout << a[i] << " ";
+		}
+        cout << endl;
+    }
 }
