39b225137d6e1494ee801f13545e21c5a64bba20
Trung <trongtrung17121@gmail.com>
2026-06-17 04:08:49 +0700

    auto-commit

--- Dsa/optap.cpp
+++ Dsa/optap.cpp
@@ -5,23 +5,31 @@ using namespace std;
 int main (){
     int t , n , k;
     cin >> t;
+    int a[105];
+    map <int,int> mp;
     while (t--){
-        cin >> n>> k;
-        int a[105];
-        for (int i = 1 ; i <= k;i++){
-			cin >> a[i];
-		}
+        cin >> n >> k;
+        for (int i = 1; i<= n ;i++){
+            cin >> a[i];
+            mp[a[i]] ++;
+    }
         int i = k;
-        while (a[i] == n - k + i) i --;
-        if (i == 0) a[i] = 0;
-        else a[i] ++;
-        while ( i < k) {
-            a[i + 1] = a[i] + 1;
-            i ++;
+        while (i >= 1 && a[i] == n- k  + i) i --;
+        if (i == 0) {
+            cout << k;
+            return 0 ;
         }
-        for (int i = 1 ; i <= k;i++){
-			cout << a[i] << " ";
-		}
-        cout << endl;
+        else {
+            a[i] ++;
+            while (i < k ){
+                a[i+1] = a[i] + 1;
+                i ++;
+            } 
+        }
+        int ans = 0;
+        for (int i = 1; i<= k ;i++){
+            if (!mp[a[i]]) ans ++;
+        }
+        cout << ans << endl;
     }
 }
