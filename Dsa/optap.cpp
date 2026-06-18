17cbd95ce1fed678e589a7d1099e7a1e774cdbda
Trung <trongtrung17121@gmail.com>
2026-06-19 02:20:55 +0700

    auto-commit

--- Dsa/optap.cpp
+++ Dsa/optap.cpp
@@ -1,61 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n ;
-struct tree {
-    int val ;
-    tree *left , *right ;
-};
+vector <int> a[1005];
+bool vs[1005];
+int parent[1005];
 
-tree *newnode (int x){
-    tree *tmp = new tree ;
-    tmp -> val = x;
-    tmp -> left = tmp -> right = NULL;
-    return tmp;
-}
-
-tree *build (vector <int> ino , vector <int> level){
-    if (ino.size () == 0) return NULL;
-    tree *root = newnode (level [0]);
-    vector <int> trai , phai , trail , phail;
-    int pos = 0;
-    while (ino[pos] != root -> val){
-        pos ++;
-    }
-    for (int i = 0 ; i< pos ; i++){
-        trai.push_back (ino[i]);
-    }
-    for (int i = pos + 1 ; i < ino.size () ; i++){
-        phai.push_back (ino[i]);
-    }
-    for (int x : level){
-        if (x == root -> val) continue;
-        if (find (trai.begin () , trai.end () , x) != trai.end()){
-            trail.push_back (x);
+void bfs (int s){
+    queue <int> q;
+    q.push (s);
+    vs[s] = true;
+    while (!q.empty ()){
+        int u = q.front();
+        q.pop ();
+        for (int v : a[u]){
+            if (!vs[v]){
+                vs[v] = true;
+                parent[v] = u;
+                q.push (v);
+            }
         }
-        else phail.push_back (x);
     }
-    root -> left = build (trai , trail);
-    root -> right = build (phai , phail);
-    return root;
-}
-void inpos (tree *root){
-    if (root == NULL) return;
-    inpos (root -> left);
-    inpos (root -> right);
-    cout << root -> val << " ";
 }
 int main (){
-    int t;
-    cin >> t;
-    while (t--){
-        cin >> n;
-        vector <int> ino (n) , level (n);
-        for (int i = 0 ; i< n ;i++) cin >> ino [i];
-        for (int i = 0 ; i< n ; i++) cin >> level [i];
-        tree *root = build (ino , level);
-        inpos (root);
+    int c;
+    cin >> c;
+    while (c--){
+        int n , m , s , t;
+        cin >> n >> m >> s >> t;
+        for (int i = 1; i<= n ;i++){
+            a[i].clear ();
+            vs[i] = false;
+            parent[i] = 0;
+        }
+        for (int i = 0 ; i< m ; i++){
+            int u , v;
+            cin >> u >> v;
+            a[u].push_back (v);
+        }
+        bfs (s);
+        if (!vs[t]){
+            cout << -1 << endl;
+            continue;
+        }
+        vector <int> path;
+         int x = t;
+        while (x != s){
+            path.push_back (x);
+            x = parent[x];
+        }
+        path.push_back (s);
+        reverse (path.begin () , path.end ());
+        for (int v : path) cout << v << " ";
         cout << endl;
+        
     }
 }
-
