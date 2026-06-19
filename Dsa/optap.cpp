5d101cef3fa6056b088f3654a0be07e8365dbef3
Trung <trongtrung17121@gmail.com>
2026-06-19 15:38:00 +0700

    auto-commit

--- Dsa/optap.cpp
+++ Dsa/optap.cpp
@@ -1,57 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector <int> a[1005];
-bool vs[1005];
-int parent[1005];
 
-void bfs (int s){
-    queue <int> q;
-    q.push (s);
-    vs[s] = true;
-    while (!q.empty ()){
-        int u = q.front();
-        q.pop ();
-        for (int v : a[u]){
-            if (!vs[v]){
-                vs[v] = true;
-                parent[v] = u;
-                q.push (v);
+vector<int> a[1005];
+int vs[1005];
+bool dfs(int u){
+    vs[u] = 1; 
+    for(int v : a[u]){
+        if(vs[v] == 1)
+            return true; 
+        if(vs[v] == 0){
+            if(dfs(v))
+                return true;
+        }
+
+    }
+    vs[u] = 2; 
+    return false;
+}
+
+int main(){
+    int T, V, E, u, v;
+    cin >> T;
+    while(T--){
+        cin >> V >> E;
+        for(int i = 1; i <= V; i++){
+            a[i].clear();
+            vs[i] = 0;
+        }
+        for(int i = 0; i < E; i++){
+            cin >> u >> v;
+         a[u].push_back(v);
+        }
+        bool ok = false;
+        for(int i = 1; i <= V; i++){
+            if(!vs[i]){
+                if(dfs(i)){
+                    ok = true;
+                    break;
+                }
             }
         }
-    }
-}
-int main (){
-    int c;
-    cin >> c;
-    while (c--){
-        int n , m , s , t;
-        cin >> n >> m >> s >> t;
-        for (int i = 1; i<= n ;i++){
-            a[i].clear ();
-            vs[i] = false;
-            parent[i] = 0;
-        }
-        for (int i = 0 ; i< m ; i++){
-            int u , v;
-            cin >> u >> v;
-            a[u].push_back (v);
-        }
-        bfs (s);
-        if (!vs[t]){
-            cout << -1 << endl;
-            continue;
-        }
-        vector <int> path;
-         int x = t;
-        while (x != s){
-            path.push_back (x);
-            x = parent[x];
-        }
-        path.push_back (s);
-        reverse (path.begin () , path.end ());
-        for (int v : path) cout << v << " ";
-        cout << endl;
-        
+        if(ok)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
     }
 }
