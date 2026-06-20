66a53322420eb94fd05e7087e9fda6a281f82bed
Trung <trongtrung17121@gmail.com>
2026-06-20 18:37:14 +0700

    auto-commit

--- Dsa/optap.cpp
+++ Dsa/optap.cpp
@@ -1,45 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int uutien (char c){
-    if (c == '^') return 3;
-    if (c == '*' || c == '/') return 2;
-    if (c == '+' || c == '-') return 1;
-    return 0;
-}
+
 
 int main (){
     int t;
     cin >> t;
     while (t--){
-        string s;
-        cin >> s;
-        stack <char>st;
-        string res ;
-        for (char c : s){
-            if (isalpha (c) || isdigit (c)) res += c;
-            else if (c == '('){
-                st.push (c);
-            }
-            else if (c == ')'){
-                while (!st.empty () && st.top () != '('){
-                    res += st.top ();
-                    st.pop ();
+       string s;
+       cin >> s;
+       stack <int> v;
+       for (int i = 0  ;i<= s.size () ;i++){
+            v.push (i + 1);
+            if (i == s.size () || s[i] == 'I'){
+                while (!v.empty ()){
+                    cout << v.top();
+                    v.pop ();
                 }
-                if (!st.empty()) st.pop ();
             }
-            else {
-                while (!st.empty () && uutien (st.top()) >= uutien (c) ){
-                    res += st.top ();
-                    st.pop ();
-                }
-                st.push (c);
-            }
-        }
-        while (!st.empty ()){
-            res += st.top ();
-            st.pop ();
-        }
-        cout << res << endl;
+       }
+       cout << endl;
     }
 }
