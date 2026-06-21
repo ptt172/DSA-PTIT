2f8bcb4acb20df02b2b5b53bb176c0c6cc4aa896
Trung <trongtrung17121@gmail.com>
2026-06-21 19:04:39 +0700

    auto-commit

--- Dsa/optap.cpp
+++ Dsa/optap.cpp
@@ -2,22 +2,42 @@
 using namespace std;
 
 
-int n;
-vector <string> res;
-void ql (int mo , int dong , string s){
-    if (s.size () == 2*n){
-        res.push_back (s);
+struct tree {
+    int val;
+    tree *right , *left ;
+    tree (int x){
+        val = x;
+        right = left = NULL;
     }
-    if (mo < n){
-        ql (mo + 1 , dong , s + "(");
-    }
-    if (dong < mo){
-        ql (mo , dong + 1 , s + ")");
-    }
+};
+tree *build (vector <string> &a , int i){
+    if (i >= a.size () || a[i] == "N") return NULL;
+    tree * root = new tree (stoi(a[i]));
+    root -> left = build (a , 2 * i + 1);
+    root -> right = build (a , 2 * i + 2);
+    return root;
+}
+void inpos (tree *root){
+    if (root == NULL) return ;
+    inpos (root -> right);
+    cout << root-> val << " ";
+    inpos (root -> left);
 }
 int main (){
-    cin >> n;
-    ql (0 , 0 , "");
-    cout << res.size () << endl;
-    for (string x : res) cout << x << endl;
+    int t;
+    cin >> t;
+    cin.ignore ();
+    while (t--){
+        string s;
+        getline (cin , s);
+        stringstream ss (s);
+        string x;
+        vector  <string>a;
+        while (ss >> x){
+            a.push_back (x);
+        }
+        tree *root = build (a , 0);
+        inpos (root);
+        cout << endl;
+    }
 }
