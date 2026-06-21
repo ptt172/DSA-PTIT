83c05d4541ac18ab54e503988c27ecfa2787c61c
Trung <trongtrung17121@gmail.com>
2026-06-21 12:50:26 +0700

    auto-commit

--- Dsa/optap.cpp
+++ Dsa/optap.cpp
@@ -2,23 +2,22 @@
 using namespace std;
 
 
-
-int main (){
-    int t;
-    cin >> t;
-    while (t--){
-       string s;
-       cin >> s;
-       stack <int> v;
-       for (int i = 0  ;i<= s.size () ;i++){
-            v.push (i + 1);
-            if (i == s.size () || s[i] == 'I'){
-                while (!v.empty ()){
-                    cout << v.top();
-                    v.pop ();
-                }
-            }
-       }
-       cout << endl;
+int n;
+vector <string> res;
+void ql (int mo , int dong , string s){
+    if (s.size () == 2*n){
+        res.push_back (s);
     }
+    if (mo < n){
+        ql (mo + 1 , dong , s + "(");
+    }
+    if (dong < mo){
+        ql (mo , dong + 1 , s + ")");
+    }
+}
+int main (){
+    cin >> n;
+    ql (0 , 0 , "");
+    cout << res.size () << endl;
+    for (string x : res) cout << x << endl;
 }
